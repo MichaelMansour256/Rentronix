cbf89c568e1f8b099953291071a66bea9609a138
michael.mansour256@yahoo.com <michael.mansour256@yahoo.com>
2020-10-15 01:32:56 +0200

    complete ui

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -6,6 +6,8 @@ import 'package:flutter_ecommerce_app/Api/ProfileApi.dart';
 import 'package:flutter_ecommerce_app/components/AppSingUp.dart';
 import 'package:flutter_ecommerce_app/screens/OrdersScreen.dart';
 import 'package:flutter_ecommerce_app/screens/WishListScreen.dart';
+import 'package:flutter_ecommerce_app/screens/EditInfo.dart';
+import 'package:flutter_ecommerce_app/screens/beASeller.dart';
 import 'package:flutter_ecommerce_app/utils/Urls.dart';
 import 'package:http/http.dart' as http;
 
@@ -55,9 +57,10 @@ class _ProfileState extends State<Profile> {
                 ),
               ),
               FlatButton(
-                onPressed: () {
-                  //TODO go to screen to put the new info
-                },
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => editYourInfo()),
+                ),
                 child: Card(
                   color: Colors.white,
                   margin:
@@ -99,9 +102,10 @@ class _ProfileState extends State<Profile> {
                 ),
               ),
               FlatButton(
-                onPressed: () {
-                  //TODO go to screen to put the new info
-                },
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => beASeller()),
+                ),
                 child: Card(
                   color: Colors.white,
                   margin:
--- lib/utils/Urls.dart
+++ lib/utils/Urls.dart
@@ -1,5 +1,5 @@
 class Urls {
-  static const ROOT_URL = "http://192.168.56.1:8888/Rentronix/public/api";
+  static const ROOT_URL = "http://192.168.0.243:45034/api";
   //http://192.168.0.243:8000
   //https://10.0.2.2:8080
 }
